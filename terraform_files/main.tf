9bc3239b5fed7f44fb2f624640f24655939a461a
Sai Ram Guthula <sairamguthula8@gmail.com>
2026-05-25 09:16:13 +0530

    Remove Azure Log Analytics Workspace resource
    Removed log analytics workspace configuration from main.tf.

--- terraform_files/main.tf
+++ terraform_files/main.tf
@@ -3,14 +3,6 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_log_analytics_workspace" "law" {
-  name                = "log-nodejs-aks"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-}
-
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
   resource_group_name = azurerm_resource_group.rg.name
@@ -32,10 +24,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
   identity {
     type = "SystemAssigned"
-  }
-
-  oms_agent {
-    log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
   }
 
   network_profile {
